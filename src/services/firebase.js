705bb80daf19835a41ab2bf749c6af24d349d508
Julian Vidal <29507199+julian-vidal@users.noreply.github.com>
2022-08-30 19:30:04 -0500

    Set the Firebase connection details in a .env file

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,14 +1,13 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-
 const firebaseConfig = {
-  apiKey: "AIzaSyBIHYb0wEHsV2MS4fLU52NasMTX-EHQFGE",
-  authDomain: "ecommerce-react-js-julian.firebaseapp.com",
-  projectId: "ecommerce-react-js-julian",
-  storageBucket: "ecommerce-react-js-julian.appspot.com",
-  messagingSenderId: "268218933197",
-  appId: "1:268218933197:web:62435bc2e04659c9996e84"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId
 };
 
 
